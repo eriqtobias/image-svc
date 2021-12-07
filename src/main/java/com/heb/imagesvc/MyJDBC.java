3877ea61feacaa85d739fe2b05d6dae67f04dbee
Eriq <zfn653@my.utsa.edu>
2021-12-07 15:31:38 -0600

    refactor error handling

--- src/main/java/com/heb/imagesvc/MyJDBC.java
+++ src/main/java/com/heb/imagesvc/MyJDBC.java
@@ -22,24 +22,18 @@ public class MyJDBC {
         }
     }
 
-    public ResultSet getAllImages(){
-        try {
+    public ResultSet getAllImages()throws SQLException {
 
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/imageschema", "root", "pass");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/imageschema", "root", "pass");
 
-            Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement();
 
-            String sqlQuery = "SELECT `images`.`ImageId`, `images`.`URL`, `images`.`label`, `images`.`detectedObjects` FROM `imageschema`.`images`;";
+        String sqlQuery = "SELECT `images`.`ImageId`, `images`.`URL`, `images`.`label`, `images`.`detectedObjects` FROM `imageschema`.`images`;";
 
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
 
-            return resultSet;
+        return resultSet;
 
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     public ResultSet getImageById(String id) throws SQLException {
@@ -54,39 +48,33 @@ public class MyJDBC {
 
     }
 
-    public ResultSet getImagesContaining(String objects){
+    public ResultSet getImagesContaining(String objects) throws SQLException{
 
         objects = objects.replace("\"", "");
         String param[] = objects.split(",");
 
-        try {
 
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/imageschema", "root", "pass");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/imageschema", "root", "pass");
 
-            Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement();
 
-            StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
 
-            stringBuilder.append("SELECT * FROM images WHERE ");
+        stringBuilder.append("SELECT * FROM images WHERE ");
 
-            String temp = "";
-            for (String object : param){
-                stringBuilder.append(temp);
-                stringBuilder.append("detectedObjects LIKE '%" + object + "%'");
-                temp = " OR ";
+        String temp = "";
+        for (String object : param){
+            stringBuilder.append(temp);
+            stringBuilder.append("detectedObjects LIKE '%" + object + "%'");
+            temp = " OR ";
 
-            }
-
-            String sqlQuery = stringBuilder.toString();
-
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-            return resultSet;
-
-        } catch (Exception e){
-            e.printStackTrace();
         }
 
-        return null;
+        String sqlQuery = stringBuilder.toString();
+
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        return resultSet;
+
     }
 }
